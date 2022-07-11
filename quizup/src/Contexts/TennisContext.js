01daf52a2cfa5947f5f5bfcef55460adce8ff83d
varun-Tad <varuntad141@gmail.com>
2022-07-11 10:11:09 +0530

    removed useEffect

--- quizup/src/Contexts/TennisContext.js
+++ quizup/src/Contexts/TennisContext.js
@@ -8,28 +8,6 @@ const TennisProvider = ({ children }) => {
 
   const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    localStorage.setItem("TennisData", JSON.stringify(TennisData));
-  }, [TennisData]);
-
-  useEffect(() => {
-    const TennisData = JSON.parse(localStorage.getItem("TennisData"));
-    if (TennisData) {
-      setTennisData(TennisData);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("score", JSON.stringify(score));
-  }, [score]);
-
-  useEffect(() => {
-    const score = JSON.parse(localStorage.getItem("score"));
-    if (score) {
-      setScore(score);
-    }
-  }, []);
-
   return (
     <TennisContext.Provider
       value={{ TennisData, setTennisData, score, setScore }}
